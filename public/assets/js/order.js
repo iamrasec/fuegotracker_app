3f278da5677af53bebad67f3a3f9a80d990e8be5
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-07-29 21:20:39 +0800

    debug 404

--- public/assets/js/order.js
+++ public/assets/js/order.js
@@ -27,7 +27,7 @@
 
       $.ajax({
         type: "POST",
-        url: "/api/order/update_order",
+        url: "/index.php/api/order/update_order",
         data: data,
         dataType: "json",
         success: function(json) {
