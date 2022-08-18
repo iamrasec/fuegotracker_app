608906c586ae77b18686998733452ee1cbad07fd
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-08-18 12:50:09 +0800

    Changed template title

--- app/Views/templates/base_dashboard.php
+++ app/Views/templates/base_dashboard.php
@@ -9,7 +9,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url(); ?>/assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="<?php echo base_url(); ?>/assets/img/favicon.png">
   <title>
-    Enjoymint Delivered | Admin Dashboard
+    Fuego Networx Tracker App
   </title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
